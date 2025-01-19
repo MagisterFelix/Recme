d11e78360efb97c4e9c4121567921b2d39642357
MagisterFelix <vladyslav.lapin.14@gmail.com>
2025-01-19 19:00:48 +0200

    Add average rating field for location

--- core/server/serializers/location.py
+++ core/server/serializers/location.py
@@ -1,16 +1,23 @@
 from collections import OrderedDict
 
-from rest_framework.serializers import ModelSerializer
+from django.db.models import Avg
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from core.server.models import Location
+from core.server.models import Location, Review
 from core.server.serializers.category import CategorySerializer
 
 
 class LocationSerializer(ModelSerializer):
 
+    rating = SerializerMethodField(method_name="get_avg_rating")
+
     class Meta:
         model = Location
         fields = "__all__"
+
+    def get_avg_rating(self, location: Location) -> float | None:
+        avg_rating = Review.objects.filter(location=location).aggregate(average=Avg("rating"))["average"]
+        return round(avg_rating, 2) if avg_rating is not None else None
 
     def to_representation(self, location: Location) -> OrderedDict:
         data = OrderedDict(super().to_representation(location))
