701daf58e2bef448658e46953704136ce07ddb9b
MagisterFelix <vladyslav.lapin.14@gmail.com>
2025-05-29 12:32:41 +0300

    Fix display of categories in admin panel

--- core/server/admin/category.py
+++ core/server/admin/category.py
@@ -8,7 +8,7 @@ from core.server.models import Category
 class CategoryAdmin(admin.ModelAdmin):
 
     def preview(self, category: Category) -> str:
-        return format_html(f"<img src=\"{category.icon.url}\" style=\"max-width: 128px; max-height: 128px\"/>")
+        return format_html(f"<img src=\"{category.icon.url}\" style=\"width: 128px; height: 128px\"/>")
 
     fieldsets = (
         (None, {
